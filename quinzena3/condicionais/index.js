bb22d4463d220b3bf49bccd60617a1e22f790c55
laecio22 <laecon22@hotmail.com>
2020-11-25 15:53:44 -0300

    Resolução dos exercícios de escrita de código sobre condicionais

--- quinzena3/condicionais/index.js
+++ quinzena3/condicionais/index.js
@@ -31,7 +31,67 @@ Esse código pega um número digitado pelo usuário  e verifica se ele é par ou
       Se o número for -10  não aparece nenhuma mensagem.
 
     c)  Sim .porque a variavel mensagem foi declarada dentro do bloco do if  portanto só é visível dentro desse bloco
-       e o comando console para mostrá-lo está fora desse bloco.
-    */
+       e o comando console para mostrá-lo está fora desse bloco.  */
+    
 
-  
+       // EXERCICIO 4
+
+        let idadeUsuario = Number(prompt("Informe a sua idade"));
+
+        if (idadeUsuario >= 18){
+          console.log("Você pode dirigir");
+        } else {
+          console.log("Você não pode dirigir");
+        } 
+
+
+
+             //          EXERCICIO 5
+
+           const turnoEstudo = prompt("Digite M para matutino , V para vespertino ou N para noturno").toUpperCase();
+
+           if (turnoEstudo === "M") {
+               console.log("Bom Dia !");
+           } else if (turnoEstudo === "V") {
+               console.log("Boa Tarde!");
+           } else if (turnoEstudo === "N"){
+             console.log("Boa Noite!");
+           } else {
+             console.log("Não existe esse  turno");
+           }
+ 
+
+
+          //           EXERCICIO 6
+
+        const turnoEstudo = prompt("Digite M para matutino , V para vespertino ou N para noturno").toUpperCase();
+
+        switch (turnoEstudo){
+          case "M":
+            console.log("Bom Dia!");
+            break;
+          case "V":
+            console.log("Boa Tarde!");
+            break;
+          case "N" :
+            console.log("Boa Noite!");
+            break;
+          default :
+             console.log("Não existe esse turno");
+             break;
+        } 
+
+    
+
+
+        //   EXERCICIO 7
+
+        const generoFilme = prompt("Qual é o gênero do filme?");
+        const precoIngresso = prompt("Qual é o valor do ingresso?");
+
+        if (generoFilme === "fantasia" && precoIngresso < 15){
+            console.log("Bom Filme!");
+        } else {
+          console.log("Escolha outro filme: (");
+        }
+        
