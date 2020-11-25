4934b3671228dca570a49faaf5483f0b6170a48c
laecio22 <laecon22@hotmail.com>
2020-11-25 11:11:11 -0300

    Resolução dos exercícios de interpretação sobre condicionais

---
+++ quinzena3/condicionais/index.js
@@ -0,0 +1,37 @@
+
+
+/* 
+               EXERCÍCIO 1
+Esse código pega um número digitado pelo usuário  e verifica se ele é par ou ímpar
+  verificando se o resto da divisão dele por 2 é igual a  0 ou não ,mostrando uma mensagem 
+  que passou no teste caso o número seja par  e uma mensagem que não passou no teste se o
+  número é ímpar
+*/      
+
+           
+
+    /*  
+                EXERCICIO 2
+    
+     a) Serve para dependendo da fruta digitada pelo usuário , mostrar uma mensagem no console
+     informando o nome dessa fruta e seu preço
+
+     b) O preço da fruta Maçã é R$ 2.25
+
+     c) O preço da fruta Pêra é R$ 5
+    */
+
+
+    /*
+              EXERCICIO 3
+    
+     a) Pega um valor digitado pelo usuário , converte no tipo Number e armazena em uma variável
+
+     b) Se o número digitado for 10 vai mostrar um  mensagem  'Esse número passou no teste' 
+      Se o número for -10  não aparece nenhuma mensagem.
+
+    c)  Sim .porque a variavel mensagem foi declarada dentro do bloco do if  portanto só é visível dentro desse bloco
+       e o comando console para mostrá-lo está fora desse bloco.
+    */
+
+  
