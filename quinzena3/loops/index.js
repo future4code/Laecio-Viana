c9885b7514f4c8537ace879301ee611ad80b2900
laecio22 <laecon22@hotmail.com>
2020-11-26 12:58:55 -0300

    Resolução dos exercicios de escrita sobre laços de repetição

---
+++ quinzena3/loops/index.js
@@ -0,0 +1,29 @@
+// EXERCICIO 1
+
+/* Faz uma repetição que vai de 0 a 4 , somando esses valores , armazenando em  uma variável
+   e mostra o resultado dela .
+
+    10
+ */
+
+ //EXERCICIO 2 
+
+  /* 
+    a) 18
+       19
+       21
+       23
+       25
+       27
+       30
+
+   b)   Não
+  */
+
+  //DESAFIO 1
+     
+ /*0
+  00
+  000
+  0000 */
+   
