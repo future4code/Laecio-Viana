4eca8336336d93ed09ea80b3850c4a3fb8ab7ce2
laecio22 <laecon22@hotmail.com>
2020-11-26 00:31:58 -0300

    Resolução dos Desafios sobre condicionais

--- quinzena3/condicionais/index.js
+++ quinzena3/condicionais/index.js
@@ -84,14 +84,123 @@ Esse código pega um número digitado pelo usuário  e verifica se ele é par ou
     
 
 
-        //   EXERCICIO 7
+        //   EXERCICIO 7 E DESAFIO 1
 
         const generoFilme = prompt("Qual é o gênero do filme?");
         const precoIngresso = prompt("Qual é o valor do ingresso?");
 
         if (generoFilme === "fantasia" && precoIngresso < 15){
-            console.log("Bom Filme!");
+            const snack = prompt("Qual  snack você quer comprar ?");
+            console.log("Bom Filme! \n ...com " , snack );
         } else {
           console.log("Escolha outro filme: (");
+        } 
+
+
+        //DESAFIO 2
+
+        const nomeCompleto = prompt("Informe seu nome completo");
+        const tipoDeJogo = prompt("Qual tipo de jogo vai assistir IN ou DO?");
+        let etapaDoJogo = prompt("Qual é a etapa do jogo SF , DT ou FI ?");
+        const categoria = Number(prompt("Escolha uma categoria 1 , 2 ,3 ou 4 "));
+        const quantidadeIngressos = Number(prompt("Informe a quantidade de ingressos  que deseja comprar"));
+        let precoIngresso ;
+        let totalPagar = 0;
+
+        switch (categoria){
+            case 1 :
+                if(etapaDoJogo === "DT"){
+                  precoIngresso = 660
+                  etapaDoJogo = "Decisão Terceiro Lugar"
+                } else if (etapaDoJogo === "SF") {
+                  precoIngresso = 1320;
+                  etapaDoJogo = "Semifinal"
+                } else if (etapaDoJogo === "FI"){
+                  precoIngresso = 1980;
+                  etapaDoJogo = "Final"
+                } else {
+                  precoIngresso = undefined;
+                }
+
+                break;
+            case 2 :
+                if(etapaDoJogo === "DT"){
+                  precoIngresso = 440
+                  etapaDoJogo = "Decisão Terceiro Lugar"
+                } else if (etapaDoJogo === "SF") {
+                  precoIngresso = 880;
+                  etapaDoJogo = "Semifinal"
+                } else if (etapaDoJogo === "FI"){
+                  precoIngresso = 1320;
+                  etapaDoJogo = "Final"
+                } else {
+                  precoIngresso = undefined;
+                }
+
+                break;
+            case 3 : 
+                if(etapaDoJogo === "DT"){
+                  precoIngresso = 330
+                } else if (etapaDoJogo === "SF") {
+                  precoIngresso = 550;
+                } else if (etapaDoJogo === "FI"){
+                  precoIngresso = 880;
+                } else {
+                  precoIngresso = undefined;
+                }
+
+                break;
+            case 4 : 
+                if(etapaDoJogo === "DT"){
+                  precoIngresso = 170
+                  etapaDoJogo = "Decisão Terceiro Lugar"
+                } else if (etapaDoJogo === "SF") {
+                  precoIngresso = 220;
+                  etapaDoJogo = "Semifinal"
+                } else if (etapaDoJogo === "FI"){
+                  precoIngresso = 330;
+                  etapaDoJogo = "Final"
+                } else {
+                  precoIngresso = undefined;
+                }
+
+                break;
+            default :
+               console.log("Opção inexistente");
+               break;
         }
+
         
+        
+        
+
+        if (tipoDeJogo === "DO") {
+           totalPagar = quantidadeIngressos * precoIngresso;
+           console.log("---------DADOS DA COMPRA----------");
+           console.log("Nome do cliente : " , nomeCompleto);
+           console.log("Tipo do jogo : Nacional ");
+           console.log("Etapa do jogo : " , etapaDoJogo);
+           console.log("Categoria : ", categoria);
+           console.log("Quantidade de ingressos : " , quantidadeIngressos , " ingressos");
+           console.log("--------Valores-------------");
+           console.log("valor do ingresso :" , " R$ " , precoIngresso);
+           console.log("Valor total " , " R$ " , totalPagar);
+
+        } else if (tipoDeJogo === "IN"){
+           
+           totalPagar = quantidadeIngressos * (precoIngresso / 4.10);
+           console.log("---------DADOS DA COMPRA----------");
+           console.log("Nome do cliente : " , nomeCompleto);
+           console.log("Tipo do jogo : Internacional ");
+           console.log("Etapa do jogo : " , etapaDoJogo);
+           console.log("Categoria : ", categoria);
+           console.log("Quantidade de ingressos : " , quantidadeIngressos , " ingressos");
+           console.log("--------Valores-------------");
+           console.log("valor do ingresso :" , " U$ " , precoIngresso / 4.10 );
+           console.log("Valor total " , " U$ " , totalPagar);
+
+        }
+         
+
+         
+
