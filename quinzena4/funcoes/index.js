0c1043fc5a148ba4a876d17750e8da4ef7c72553
laecio22 <laecon22@hotmail.com>
2020-12-01 10:27:07 -0300

    Resolução dos exercícios de escrita de código  sobre funções

---
+++ quinzena4/funcoes/index.js
@@ -0,0 +1,18 @@
+//EXERCICIO 1
+
+//a) vai imprimir o número 10 e o número 50
+
+//b) Não apareceria nenhuma mensagem no console
+
+
+//EXERCICIO 2
+
+//a) Darvas e Goli
+
+//b) Amanda  e Caio
+
+//EXERCICIO 3
+
+// A função recebe um array como parametro, cria um novo array  , percorre o array passado como argumento verificando
+// se o elemento é par  e adicionando no novo array o produto dele por ele mesmo  e dá como retorno esse novo array
+
