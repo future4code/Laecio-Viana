21db03bdfbd97daa269ad7b8b7a8cbf6178b3b1f
laecio22 <laecon22@hotmail.com>
2020-12-01 15:03:01 -0300

    Finalização dos exercícios sobre funções

--- quinzena4/funcoes/index.js
+++ quinzena4/funcoes/index.js
@@ -18,6 +18,113 @@
 // novo nome retornaArrayComElementosPares
 
 
+//EXERCICIO 4
+
+//a) 
+   function mostrarMensagemSobreMim(){
+       console.log("Eu sou Laécio, tenho 27 anos , moro em Altos- Piauí e sou estudante")
+   }
+
+   mostrarMensagemSobreMim()
+ 
+
+ //b)
+      const mostrarInformacoesSobreMim = (nome, idade , cidade, souEstudante) =>{
+          if (souEstudante){
+            console.log("Eu sou " + nome + " , tenho " + idade + " anos , moro em " + cidade + " e sou estudante " )
+          } else {
+            console.log("Eu sou " + nome + " , tenho " + idade + " anos , moro em " + cidade + " e  não sou estudante " )
+          }
+      }
+
+      mostrarInformacoesSobreMim("Jorge", 10, "Fortaleza-CE", false)
 
 
+      //EXERCICIO 5
 
+//a)
+     function  retornaSomaDeDoisNumeros(numero1, numero2){
+          const soma = numero1 + numero2
+          return soma
+     }
+
+     console.log(retornaSomaDeDoisNumeros(7,8))
+     
+//b) 
+    function verificaSePrimeiroNumeroEMaiorOuIgualAoSegundoNumero(valor1, valor2){
+          return valor1 >= valor2
+    }
+
+    console.log(verificaSePrimeiroNumeroEMaiorOuIgualAoSegundoNumero(6,9))
+   
+//c) 
+
+   const imprimirMensagemDezVezes = (mensagem) => {
+       for  (let i = 0 ; i <= 9 ; i++) {
+           console.log( mensagem) 
+       }
+   }
+
+
+   imprimirMensagemDezVezes("Bom dia !")
+
+
+        //EXERCICIO 6
+ 
+  //a)
+      const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+
+      function retornaQuantidadeElementosArray (array){
+           return array.length
+      }
+
+      console.log(retornaQuantidadeElementosArray(array), "elementos")
+
+
+//b)
+     
+    function verificaSeNumeroEParOuNao (numero) {
+
+        let ePar 
+
+        if (numero % 2 === 0){
+            ePar = true
+        } else {
+            ePar = false
+        }
+
+        return ePar
+
+    }
+
+    console.log(verificaSeNumeroEParOuNao(10))
+
+  //c)
+
+  function calculaQuantidadeNumerosParesDoArray (array){
+       let quantidadeNumerosPares = 0
+
+       for (const item of array){
+           if (item % 2 === 0) {
+              quantidadeNumerosPares++
+           }
+       }
+
+       return quantidadeNumerosPares
+  }
+
+  console.log(calculaQuantidadeNumerosParesDoArray(array), "Números pares")
+
+  //d)
+       function calculaQuantidadeNumerosParesDoArray (array){
+                let quantidadeNumerosPares = 0
+
+                for (const item of array){
+                    if (verificaSeNumeroEParOuNao(item)) {
+                       quantidadeNumerosPares++
+                    }
+                }
+
+                return quantidadeNumerosPares
+            }
+       console.log(calculaQuantidadeNumerosParesDoArray(array), "Números pares")
