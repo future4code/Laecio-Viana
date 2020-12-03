01106b8c632f02fef9978b5d2b92453bb38ea408
laecio22 <laecon22@hotmail.com>
2020-12-03 20:38:43 -0300

    Implementação da funcionalidade de ao clicar em uma tarefa ela deve ficar riscada

--- quinzena4/planner/planner/index.js
+++ quinzena4/planner/planner/index.js
@@ -1,18 +1,25 @@
+const tarefa = document.getElementById("tarefa")
+const diaSemanaEscolhido = document.getElementById("dias-semana")
+
 const adicionarTarefa = () => {
-   const tarefa = document.getElementById("tarefa").value
    
-   const diaSemanaEscolhido = document.getElementById("dias-semana").value
 
-   if (tarefa !== "" ) {
+   if (tarefa.value !== "" ) {
 
-      const diaRealizacaoTarefa = document.getElementById(diaSemanaEscolhido)
-      diaRealizacaoTarefa.innerHTML += `<p>${tarefa}</p>`
+      const diaRealizacaoTarefa = document.getElementById(diaSemanaEscolhido.value)
+      diaRealizacaoTarefa.innerHTML += `<p onclick ="riscarTarefa(this)" >${tarefa.value}</p>`
    
       document.getElementById("tarefa").value = ""
+
    }  else {
          alert("Informe uma tarefa a ser realizada")
-   }
+   
+      }
   
    
    
+}
+
+function riscarTarefa(obj){
+   obj.innerHTML = `<p class="texto-riscado">${obj.innerHTML}</p>`
 }
