4eebcaff50907bcea8bba7eb27464e3b4b5dcbee
laecio22 <laecon22@hotmail.com>
2020-12-03 18:54:11 -0300

    Implementação de uma mensagem de alert ao usuário se ele clicar no botão sem digitar nenhuma tarefa

--- quinzena4/planner/planner/index.js
+++ quinzena4/planner/planner/index.js
@@ -2,11 +2,17 @@ const adicionarTarefa = () => {
    const tarefa = document.getElementById("tarefa").value
    
    const diaSemanaEscolhido = document.getElementById("dias-semana").value
+
+   if (tarefa !== "" ) {
+
+      const diaRealizacaoTarefa = document.getElementById(diaSemanaEscolhido)
+      diaRealizacaoTarefa.innerHTML += `<p>${tarefa}</p>`
+   
+      document.getElementById("tarefa").value = ""
+   }  else {
+         alert("Informe uma tarefa a ser realizada")
+   }
   
    
-   const diaRealizacaoTarefa = document.getElementById(diaSemanaEscolhido)
-   diaRealizacaoTarefa.innerHTML += `<p>${tarefa}</p>`
-
-   document.getElementById("tarefa").value = ""
    
 }
