b5df9f703c94880abf4b3dc6fd994552a6a1d8f7
laecio22 <laecon22@hotmail.com>
2020-12-04 14:23:19 -0300

    Implementação da funcionalidade de criar um botão que ao ser clicado limpa todas as tarefas da agenda

--- quinzena4/planner/planner/index.js
+++ quinzena4/planner/planner/index.js
@@ -1,13 +1,19 @@
 const tarefa = document.getElementById("tarefa")
 const diaSemanaEscolhido = document.getElementById("dias-semana")
+let quantidadeTarefas = 0
+
 
 const adicionarTarefa = () => {
+   
+   const diaRealizacaoTarefa = document.getElementById(diaSemanaEscolhido.value)
+  
    
 
    if (tarefa.value !== "" ) {
 
-      const diaRealizacaoTarefa = document.getElementById(diaSemanaEscolhido.value)
       diaRealizacaoTarefa.innerHTML += `<p onclick ="riscarTarefa(this)" >${tarefa.value}</p>`
+      quantidadeTarefas += 1
+     
    
       document.getElementById("tarefa").value = ""
 
@@ -23,3 +29,20 @@ const adicionarTarefa = () => {
 function riscarTarefa(obj){
    obj.innerHTML = `<p class="texto-riscado">${obj.innerHTML}</p>`
 }
+
+function limparTarefas(){
+
+   const tarefasCriadas = document.getElementsByTagName("p")
+
+   
+   for (let index = 0 ; index < quantidadeTarefas; index++){
+       tarefasCriadas[index].innerHTML = ""
+   }
+
+   
+
+
+   
+}
+
+
