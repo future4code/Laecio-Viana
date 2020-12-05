fd5a37186b738db907fea2f82909bedb28e1ac61
laecio22 <laecon22@hotmail.com>
2020-12-05 10:49:57 -0300

    Resolução do segundo exercício de interpretação de código

--- quinzena4/lista-de-exercicios/index.js
+++ quinzena4/lista-de-exercicios/index.js
@@ -9,6 +9,12 @@ function resolverExerciciosInterpretacaoCodigo () {
  */
 
     //exercicio 2
-
+   /* A função usa como parâmetros o tipo de investimento a ser feito  e o valor desse investimento  e vai retornar o novo
+   valor após realização desse investimento.  Dentro da função é criada uma variável que representa o novo valor 
+   após investimento , é usada uma estrutura de decisão switch case que dependendo do tipo de investimento 
+   o novo valor após investimento vai receber o produto do valor passado por parâmetro por uma determinada taxa.
+  
+   O primeiro console.log vai mostrar o valor 165 e no segundo console.log vai dar um alert com a mensagem 'tipo de investimento
+   informado incorreto'  e mostrar a  mensagem  undefined */
 
 }
