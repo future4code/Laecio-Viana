2c56fdfc4afc3db774940a8c6a497918664d3f5e
laecio22 <laecon22@hotmail.com>
2020-12-05 10:30:15 -0300

    Resolução do primeiro exercício de interpretação de código

---
+++ quinzena4/lista-de-exercicios/index.js
@@ -0,0 +1,14 @@
+function resolverExerciciosInterpretacaoCodigo () {
+    //exercicio 1
+
+
+    /* A função vai converter  o valor do dinheiro de dólares  em reais , passando como parâmetro o valor em dólares,
+    O usuário informa a cotacão  através de um prompt convertendo esse valor em número e armazenando em uma variável 
+    e depois retorna o produto da cotação pelo valor em dólares. Nesse caso o console.log vai mostrar como resultado
+    o valor de 100 vezes a cotação digitada pelo usuário.
+ */
+
+    //exercicio 2
+
+
+}
