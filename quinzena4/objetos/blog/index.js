99287f01ffcb1e96d758eb72612f5f053426e2d6
laecio22 <laecon22@hotmail.com>
2020-12-08 14:10:47 -0300

    Criação de função para adicionar o objeto post em um array  e outra função para limpar os campos do formulário

--- quinzena4/objetos/blog/index.js
+++ quinzena4/objetos/blog/index.js
@@ -2,6 +2,8 @@ let tituloDigitado = document.getElementById("titulo-post")
 let autorDigitado = document.getElementById("autor-post")
 let textoDigitado = document.getElementById("conteudo-post")
 
+const arrayPosts = []
+
 function criarPost() {
    const post = {
        titulo : tituloDigitado.value,
@@ -9,5 +11,23 @@ function criarPost() {
        conteudo : textoDigitado.value
    }
 
+   adicionarPostNoArray(post)
+   console.log(arrayPosts)
+   limparCamposFormulario()
+
    
+     
 }
+
+function adicionarPostNoArray (post){
+     arrayPosts.push(post)   
+}
+
+function limparCamposFormulario () {
+    tituloDigitado.value = ""
+    autorDigitado.value = ""
+    textoDigitado.value = ""
+}
+
+
+   
