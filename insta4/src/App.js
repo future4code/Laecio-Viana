22fee144d5f026bc6468945d5a68a3cf468f8ff7
laecio22 <laecon22@hotmail.com>
2021-01-08 10:53:37 -0300

    Criação de array de posts e transformação em array de componentes e renderização na tela

--- insta4/src/App.js
+++ insta4/src/App.js
@@ -1,30 +1,43 @@
 import React from 'react';
 import './App.css';
 import Post from './components/Post/Post';
+import styled from 'styled-components'
 
 class App extends React.Component {
   render() {
+    const posts = [
+      {
+          nomeUsuario:'paulinha',
+          fotoUsuario :'https://picsum.photos/50/50',
+          fotoPost:'https://picsum.photos/200/150'
+      },
+      
+      {
+          nomeUsuario:'Carlos',
+          fotoUsuario: 'https://picsum.photos/id/1023/367/267',
+          fotoPost:'https://picsum.photos/id/1022/367/267'
+
+      },
+
+      {
+          nomeUsuario:'Maria',
+          fotoUsuario:'https://picsum.photos/id/1025/367/267',
+          fotoPost:'https://picsum.photos/id/1021/367/267'
+      }
+    ]
+
+    const listaPosts = posts.map((post)=>{
+         return ( <Post nomeUsuario = {post.nomeUsuario} fotoUsuario={post.fotoUsuario} fotoPost={post.fotoPost}>
+                   
+                 </Post>
+
+         )
+    })
+
     return (
+
       <div className={'app-container'}>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-
-         <Post
-          nomeUsuario={'Carlos'}
-          fotoUsuario={'https://picsum.photos/id/1023/367/267'}
-          fotoPost={'https://picsum.photos/id/1022/367/267'}
-        />
-
-        <Post
-          nomeUsuario={'Maria'}
-          fotoUsuario={'https://picsum.photos/id/1025/367/267'}
-          fotoPost={'https://picsum.photos/id/1021/367/267'}
-        />
-        
-        
+          {listaPosts}
         
       </div>
     );
