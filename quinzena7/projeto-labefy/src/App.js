3b28dab1ba89e6cee9512b3ed0fa217885d47226
laecio22 <laecon22@hotmail.com>
2021-01-31 15:38:28 -0300

    Funcionalidade de adicionar playlist

---
+++ quinzena7/projeto-labefy/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import CadastroPlaylist from './components/CadastroPlaylist'
+
+class App extends React.Component {
+  render(){
+
+    return (
+      <div>
+          <h1>Laecio</h1>
+          <CadastroPlaylist/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ quinzena7/projeto-labefy/src/components/CadastroPlaylist.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import axios from 'axios'
+
+class CadastroPlaylist extends React.Component {
+
+    state = {
+        inputNomePlaylist : ""
+    }
+
+    onChangeNomePlaylist = (event) =>{
+
+        this.setState({inputNomePlaylist:event.target.value})
+
+    }
+
+    criarPlaylist = (nomePlaylist) =>{
+       const body = {
+           name: nomePlaylist
+       }
+
+       axios.post('https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists', body,{
+           headers:{
+               Authorization: "laecio-conceicao-muyembe"
+           }
+       }).then(()=>{
+           alert("Playlist cadastrada com sucesso")
+       })
+
+       .catch(()=>{
+           alert("Não foi possível cadastrar playlist")
+       })
+
+       this.setState({inputNomePlaylist:""})
+    }
+  render(){
+
+    return (
+      <div>
+          <h1>Cadastro de Playlist</h1>
+
+          <form>
+              <div>
+                  <label>Nome da Playlist:</label>
+                  <input type="text" placeholder="Informe a sua playlist" value={this.state.inputNomePlaylist} onChange={this.onChangeNomePlaylist}/>
+                  
+              </div>
+          </form>
+          <button onClick={()=> this.criarPlaylist(this.state.inputNomePlaylist)}>Cadastrar</button>
+      </div>
+    );
+  }
+}
+
+export default CadastroPlaylist;
