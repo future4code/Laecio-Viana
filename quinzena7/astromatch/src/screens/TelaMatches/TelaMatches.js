206216ae77e5af9ba28a4ffab8fb698bd18be7f7
laecio22 <laecon22@hotmail.com>
2021-02-25 23:55:52 -0300

    Acessar API e mostrar na tela a foto e o nome de cada match da lista de matches do usuário

--- quinzena7/astromatch/src/screens/TelaMatches/TelaMatches.js
+++ quinzena7/astromatch/src/screens/TelaMatches/TelaMatches.js
@@ -1,11 +1,37 @@
-import React from 'react'
-import {ContainerTelaMatches}  from './styled'
+import React,{useState,useEffect} from 'react'
+import {ContainerTelaMatches, ListaMatches, Match, ImagemPessoaMatch, NomePessoaMatch}  from './styled'
 import Header from '../../components/Header/Header'
+import axios from 'axios'
+import {BASE_URL} from '../../constants/urls'
 
 function TelaMatches(props) {
+  
+  const[matches, setMatches] = useState([])
+  useEffect(()=>{
+
+     axios.get(`${BASE_URL}/matches`)
+     .then(resposta =>{
+       setMatches(resposta.data.matches)     
+       
+     })
+     .catch(erro =>{
+       console.log(erro)
+     })
+     
+  },[])  
+
   return (
+    
     <ContainerTelaMatches>      
         <Header voltaTela={props.voltaTela}/>
+        <ListaMatches>
+            {matches.map((match)=>{
+               return <Match>
+                         <ImagemPessoaMatch src={match.photo}/>                 
+                         <NomePessoaMatch>{match.name}</NomePessoaMatch>                         
+                      </Match>
+            })}
+        </ListaMatches>
     </ContainerTelaMatches>
   );
 }
--- quinzena7/astromatch/src/screens/TelaMatches/styled.js
+++ quinzena7/astromatch/src/screens/TelaMatches/styled.js
@@ -6,7 +6,23 @@ export const ContainerTelaMatches = styled.div`
     margin: 30px auto;
     border: 1px solid #000;
     display: grid;
-    grid-template-rows: 80px 5fr ;
-   
+    grid-template-rows: 80px 5fr ;   
     
+`
+export const ListaMatches = styled.div`
+
+`
+export const Match = styled.div`
+    display:flex;
+    align-items: center;
+    padding: 10px;
+`
+export const ImagemPessoaMatch = styled.img`
+     width:50px;
+     height: 50px;  
+     margin-right:15px;
+             
+`
+export const NomePessoaMatch = styled.div`
+
 `
