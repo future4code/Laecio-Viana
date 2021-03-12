ab9545096daf7c05653e5e9010831e3bca629792
laecio22 <laecon22@hotmail.com>
2021-03-11 21:28:27 -0300

    Mostrar a lista das viagens para o administrador escolher qual deseja detalhar

--- quinzena9/labex/src/pages/TripDetailsPage/TripDetailsPage.js
+++ quinzena9/labex/src/pages/TripDetailsPage/TripDetailsPage.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import {useHistory} from 'react-router-dom'
-import {ContainerTripDetailsPage} from './styled'
+import {ContainerTripDetailsPage, ContainerTrips, ItemTrip} from './styled'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import Main from '../../components/Main/Main'
 import {logout} from '../../routers/Coordinator'
 import useProtectedPage from '../../hooks/useProtectedPage'
+import axios from 'axios'
+import {BASE_URL} from '../../constants/urls'
+
 
 function TripDetailsPage() {
-  const history = useHistory()  
+  const history = useHistory()
+  const[trips, setTrips] = useState([])  
+
+  useEffect(()=>{
+    axios.get(`${BASE_URL}/trips`).then((response)=>{
+            setTrips(response.data.trips)
+        }).catch((error)=>{
+            alert(error)
+        })
+  },[])  
   useProtectedPage()
 
   return (
@@ -16,6 +28,16 @@ function TripDetailsPage() {
        <Header redirectPage={()=>logout(history)} titleRedirect={"Logout"}/>  
        <Main>
             <h2>Lista Viagens</h2>
+            <ContainerTrips>
+              
+                {trips.map((trip)=>{
+                  return <ItemTrip>
+                              <p>Nome</p>
+                              <p>{trip.name}</p>
+                              <button>Detalhar Viagem</button>
+                          </ItemTrip>
+                })}
+            </ContainerTrips>
        </Main>
        <Footer/>      
     </ContainerTripDetailsPage>
--- quinzena9/labex/src/pages/TripDetailsPage/styled.js
+++ quinzena9/labex/src/pages/TripDetailsPage/styled.js
@@ -4,6 +4,17 @@ export const ContainerTripDetailsPage = styled.div`
         width: 100vw;
         height:100vh;       
 `
+export const ContainerTrips = styled.div`      
+       display:grid;
+       justify-items: center;
+`
 
+export const ItemTrip = styled.div`
+      display:grid;
+      grid-template-columns:  250px 250px 1fr 1fr   ;
+      justify-items: center;   
+      margin-top: 15px;       
+      
+  `   
 
 
